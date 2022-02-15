9577fda58d14fdbdd369d597b42d101750e5d366
Michael Bee <bee.michael.02@gmail.com>
2022-02-15 10:32:41 -0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+//First page: HS and Timer are visible. Title, game description and rules with time penalty, start game button. 
+        //need event listener on the start button that clears the center display, shows the first question, and starts the timer.
+
+//Questions pages: need function that populates the question in h2, adds 4 buttons with answers, all buttons need their own event listener that triggers an if/else. 
+        //The wrong answers trigger the wrong function, which displays wrong and takes time off timer, and loads the next question. 
+        //The right answers displays right and loads the next question.
+
+//Finishing last question or time running out triggers all done page to display with your high score, add initials input, and submit button.
+        //Submit button adds initials and high score to local memory and displays the high score listings
+
+// High score listings displays and enumerates from local memory
+        //clear button wipes local memory and re-displays high score page
+        //play again button starts a new game

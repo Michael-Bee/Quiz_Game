bf56b1849ca88eb9beab5ef6ca488486b16f3ed1
Michael Bee <bee.michael.02@gmail.com>
2022-02-15 19:58:53 -0600

    Pseudo-coded an outline, set up some objects in the HTML and some initial style, started working on the first function that starts the game and the timer.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,34 @@
+var start = document.querySelector("#startButton");
+start.setAttribute("style", "visibility: visible")
+
+
+
+function renderQuestion(event){
+        //stop bubbling
+        event.stopPropagation();
+
+        console.log("start clicked")
+
+        start.setAttribute("style", "visibility: hidden")
+        bigText.setAttribute("style", "visibility: hidden")
+        timer.setAttribute("style", "visibility: visible")
+        //start timer
+}
+
+
+//Game Over function - when all questions finished or time runs out, displays "Your score is ____" and and input box to enter initials. Submit button loads high score page (same as View Highscores button)
+
+
+start.addEventListener("click", renderQuestion);
+
+
+
+
 //First page: HS and Timer are visible. Title, game description and rules with time penalty, start game button. 
-        //need event listener on the start button that clears the center display, shows the first question, and starts the timer.
 
 //Questions pages: need function that populates the question in h2, adds 4 buttons with answers, all buttons need their own event listener that triggers an if/else. 
-        //The wrong answers trigger the wrong function, which displays wrong and takes time off timer, and loads the next question. 
-        //The right answers displays right and loads the next question.
+        //A false answer triggers display of wrong, takes 5 seconds off timer, and loads the next question. 
+        //A true answer displays right and loads the next question.
 
 //Finishing last question or time running out triggers all done page to display with your high score, add initials input, and submit button.
         //Submit button adds initials and high score to local memory and displays the high score listings
